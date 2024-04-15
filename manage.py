26daadaa9db3513bb66c6b686bc6b1f170468077
manoty <kevinnoty21@gmail.com>
2024-04-15 10:01:37 +0300

    used if,elif and else statements to solve different python problems

---
+++ manage.py
@@ -0,0 +1,23 @@
+
+"""
+this module determines life stages through different ages
+"""
+Age = 35
+
+if Age == 22:
+    print("Time to marry")
+elif Age <= 26:
+    print("Grind for life awaits no man!")
+elif Age >= 30:
+    print("Enjoy life! You made it")
+else:
+    print("Continue grinding, almost there!")
+
+
+x = 8
+y = 15
+
+if x>7 or y%2==0:
+    print("atleast one condition is satisfied")
+elif x<=3 and y%2==1:
+    print("Neither of the condition is satisfied")
